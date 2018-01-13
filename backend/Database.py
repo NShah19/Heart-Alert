bd828168c69abd1ae3e4a6c030b0dc550dd031a5
NShah19 <nilay.shah19@gmail.com>
2018-01-13 14:45:49 -0800

    set up database classes and some code for placing phone calls

---
+++ backend/Database.py
@@ -0,0 +1,26 @@
+from mongoengine import *
+import random
+import string
+
+connect('mongoengine_test', host='localhost', port=27017)
+
+def getAccessCode():
+    return(''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(8)))
+
+class Person(Document):
+    name = StringField(required=True, max_length=70)
+    phone = IntField(required=True, max_length=10)
+
+class User(Document):
+    basicInfo = ReferenceField(Person)
+    email = EmailField()
+    accessCode = IntField(required=True, max_length=8)
+    
+class MedicalInfo(Document):
+    age = IntField(required=True, max_length=3)
+    userHistory = BooleanField()
+    familyHistory = BooleanField()
+    medications = ListField(StringField(max_length=20))
+
+x = getAccessCode()
+print(x)
